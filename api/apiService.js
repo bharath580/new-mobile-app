71831e51c65f8ed542d29497e6d428893005e1de
bharath580 <bharath@kabadiwallaconnect.in>
2024-12-18 12:19:10 +0530

    set header

--- api/apiService.js
+++ api/apiService.js
@@ -1,9 +1,38 @@
-
-import axios from "axios";
+import axios from 'axios';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export const api = axios.create({
-    baseURL: 'http://13.202.98.144:2000/api/',
-    // baseURL: 'http://192.168.1.2:2000/api/',
-    timeout: 10000,
-    headers: {'X-Custom-Header': 'foobar',}
-  });
+  // baseURL: 'http://13.202.98.144:2000/api/',
+  baseURL: 'http://13.202.98.144:2000/api/',
+  timeout: 10000,
+  headers: {'X-Custom-Header': 'foobar'},
+});
+
+export const setAuthToken = async () => {
+  const userData = await AsyncStorage.getItem('user');
+  console.log('userData', userData);
+  if (userData) {
+    try {
+      const user = JSON.parse(userData);
+      const {access_token} = user[0];
+
+      if (access_token) {
+        const headers = {
+          ...api.defaults.headers.common,
+          Authorization: `Bearer ${access_token}`,
+        };
+        api.defaults.headers.common = headers;
+      } else {
+        delete api.defaults.headers.common['Authorization'];
+      }
+    } catch (error) {
+      console.error('Error parsing user data:', error);
+      delete api.defaults.headers.common['Authorization'];
+    }
+  } else {
+    delete api.defaults.headers.common['Authorization'];
+  }
+};
+
+// Call setAuthToken to set initial token
+setAuthToken();
--- screens/segregation/Segregation.jsx
+++ screens/segregation/Segregation.jsx
@@ -7,72 +7,61 @@ import { Picker } from '@react-native-picker/picker';
 import { useFocusEffect } from '@react-navigation/native';
 import Loader from '../Loader';
 
-const Segregation = ({navigation}) => {
+const Segregation = ({ navigation }) => {
   const dispatch = useDispatch();
 
-  const {segregationBatchList,isLoading} = useSelector(state => state.segregation);
-  const [isChecked, setIsChecked] = useState(false);
-  const [batch, setBatch] = useState();
-  // useEffect(() => {
-    
-  //   console.log('segregationBatchList', segregationBatchList);
-  //   // console.log("use effect",materialList[0].purchaseAndMaterial)
-  // }, [dispatch]);
+  const { segregationBatchList, isLoading } = useSelector(state => state.segregation);
+  const [batch, setBatch] = useState(null);
+
   useFocusEffect(
     useCallback(() => {
-        // Fetch or refresh data here
-        dispatch(getSegregationBatchList()); // Replace with the appropriate action to reload your data
-        console.log('segregationBatchList', segregationBatchList);
+      // Fetch or refresh data here
+      dispatch(getSegregationBatchList());
     }, [dispatch])
-);
-if(isLoading){
-  return <Loader/>;
-  
-}
-  const handleMaterialChange = (value, index) => {
-    console.log('value', value);
-    // const {po_id,material_id}=value
+  );
+
+  if (isLoading) {
+    return <Loader />;
+  }
+
+  const handleMaterialChange = (value) => {
     setBatch(value);
   };
+
   return (
     <>
-    <View className="mx-2 my-4 flex-1  rounded p-4 flex flex-col">
-      <View className="border-b">
-        <View className="flex-row items-center justify-between">
-          <Text className="text-lg font-semibold">Batch Id</Text>
+      <View className="mx-2 my-4 flex-1 rounded p-4 flex flex-col">
+        <View className="border-b">
+          <View className="flex-row items-center justify-between">
+            <Text className="text-lg font-semibold">Batch Id</Text>
+          </View>
+          <Picker
+            selectedValue={batch}
+            onValueChange={handleMaterialChange}
+          >
+            {segregationBatchList.length>0 &&
+              segregationBatchList.map((data) => (
+                <Picker.Item
+                  key={data.batch_id}  // Use batch_id as a unique key
+                  label={`${data.display_id} (${data.totalQuantity}kg)`}
+                  value={data.batch_id}
+                />
+              ))}
+          </Picker>
         </View>
-        <Picker
-          selectedValue={batch}
-          onValueChange={value => handleMaterialChange(value)}>
-          {segregationBatchList &&
-            segregationBatchList.map((data, i) => (
-              <Picker.Item
-                key={i}
-                name={i}
-                label={`${data.display_id} (${data.totalQuantity}kg)`}
-                value={data.batch_id}
-              />
-            ))}
-        </Picker>
       </View>
-     
-    </View>
 
-   
+      <TouchableOpacity>
+        <Button
+          className="bg-green-500 text-lg hover:bg-green-700 font-bold py-2 px-3 rounded w-full"
+          textColor="white"
+          onPress={() => navigation.navigate('SegregationDetail', { id: batch })}
+        >
+          Next {'>'}
+        </Button>
+      </TouchableOpacity>
+    </>
+  );
+};
 
-       <TouchableOpacity>
-       <Button
-         className="bg-green-500 text-lg hover:bg-green-700
-font-bold py-2 px-3 rounded w-full "
-         textColor="white"  onPress={() =>
-          navigation.navigate('SegregationDetail', {id: batch})
-        }>
-     
-         Next {'>'}
-       </Button>
-     </TouchableOpacity>
-     </>
-  )
-}
-
-export default Segregation
+export default Segregation;
